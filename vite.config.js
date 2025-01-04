e4a3a53c2af4ffbc7fdfcafcf6c394e6dcbd7f4b
João Pedro Lourenço dos Santos <joao.santos1776@etec.sp.gov.br>
2025-01-04 02:11:11 -0300

    funcionaaa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/JoaoPedroPortfolio.github.io/", // Substitua pelo nome do repositório
+  base: "/portfolio", // Substitua pelo nome exato do repositório
 });
