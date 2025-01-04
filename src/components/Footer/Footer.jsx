c9be6bf249dbcadcaf3b8c98f2fab79f08d576ed
João Pedro Lourenço dos Santos <joao.santos1776@etec.sp.gov.br>
2025-01-04 02:16:06 -0300

    pequenas mudanças visuais

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,16 +3,12 @@ import ItensFooter from "./ItensFooter";
 
 const Footer = () => {
   return (
-    <div className="w-full max-w-5xl h-auto py-5 m-auto bg-slate-800 flex flex-col justify-center gap-10 rounded-tr-3xl rounded-tl-3xl relative">
+    <div className="w-full max-w-5xl h-auto py-5 m-auto bg-slate-800/10 flex flex-col md:flex-row justify-center gap-10 rounded-tr-3xl rounded-tl-3xl relative">
       <ItensFooter
         logo="fa fa-envelope"
         info="joaopedrolourencocontato@gmail.com"
       />
       <ItensFooter logo="fa fa-phone" info="(14) 99757-3336" />
-
-      <p className="relative bottom-0 text-gray-500 font-semibold text-center">
-        Site feito por mim mesmo, João Pedro Lourenço!
-      </p>
     </div>
   );
 };
--- src/components/Footer/ItensFooter.jsx
+++ src/components/Footer/ItensFooter.jsx
@@ -4,10 +4,12 @@ const ItensFooter = ({ logo, info }) => {
   return (
     <div>
       <div className="flex flex-col items-center justify-center gap-2">
-        <i
-          className={logo}
-          style={{ fontSize: "30px", color: "white", opacity: "0.5" }}
-        ></i>
+        <div className="w-[50px] h-[50px] flex flex-col items-center justify-center rounded-xl bg-slate-800/30 border-[1px] border-slate-400/30 hover:scale-105 hover:shadow-lg duration-200 cursor-pointer">
+          <i
+            className={logo}
+            style={{ fontSize: "30px", color: "white", opacity: "0.5" }}
+          ></i>
+        </div>
         <p className="text-white text-sm font-medium opacity-50">{info}</p>
       </div>
     </div>
